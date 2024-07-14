3d8a46f8277c8615b8d292f9b9cfd949f332e25f
Anchal Kumari <144810484+anchalkumari1@users.noreply.github.com>
2024-07-14 13:46:07 +0000

    Some methods added.

--- Tree/BST.java
+++ Tree/BST.java
@@ -1,15 +1,15 @@
 class Node{
 
-        int data;
-        Node left;
-        Node right;
+    int data;
+    Node left;
+    Node right;
 
-        public Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
+    public Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
+}
 
 class BSTree{
     
@@ -53,11 +53,66 @@ class BSTree{
             return true;
         }
     }
+
+    Node delete(Node root, int val){
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else if(root.data < val){
+            root.right = delete(root.right, val);
+        }
+        else{
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            Node is = inOrderSuccessor(root.right);
+            root.data = is.data;
+            root.right = delete(root.right, is.data); 
+        }
+        return root;
+    }
+
+    Node inOrderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    void printInRange(Node root, int k1, int k2){
+        if(root == null){
+            return;
+        }
+
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }
+
+        else if(root.data < k1){
+            printInRange(root.right, k1, k2);
+        }
+
+        else if(root.data > k2){
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    
 }
 
 class BST{
     public static void main(String args[]){
-        int values[] = {5, 1, 3, 4, 2, 7};
+        int values[] = {3, 4, 5, 6, 1, 11, 10, 14, 8};
 
         BSTree tree = new BSTree();
         Node root = null;
@@ -74,5 +129,12 @@ class BST{
         }else{
             System.out.println("Not found.");
         }
+
+        tree.delete(root, 5);
+        tree.inOrder(root);
+        System.out.println();
+
+        tree.printInRange(root, 5, 12);
+        
     }
 }
